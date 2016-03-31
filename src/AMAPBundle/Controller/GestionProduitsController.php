a92b88149633d71a2c7c64bf068922ab408b5d7f
Cocdesbois <corentin.fougeret@laposte.net>
2016-03-31 11:09:47 +0200

    Corentin
    Ajout du lien gestion produit et création de son controlleur et sa view

---
+++ src/AMAPBundle/Controller/GestionProduitsController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace AMAPBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use \Symfony\Component\Form\Extension\Core\Type\TextType;
+use \Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use \Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use AMAPBundle\Entity\Produit;
+class GestionProduitsController extends Controller
+{
+    public function indexAction(\Symfony\Component\HttpFoundation\Request $request)
+    {
+//        $em =  $this->getDoctrine()->getManager();
+//       
+//        
+//        $form = $this->createFormBuilder()
+//                ->setMethod('POST') 
+//                ->add('libelleProduit', TextType::class)
+//                ->add('quantite', IntegerType::class)
+//                ->add('valider', SubmitType::class)
+//                ->getForm();
+//        if($form->handleRequest($request)->isSubmitted()){
+//            $prod = new Produit();
+//            $data = $form->getData();
+//            $prod->setLibelle($data['libelleProduit']);
+//            $prod->setQuantite($data['quantite']);
+//            
+//            $em->persist($prod);
+//            $em->flush();
+//                    
+//        }
+//         
+//        $produit = $em->getRepository('AMAPBundle:Produit')
+//                ->findAll();
+        return $this->render('AMAPBundle:GestionProduits:gestionProduits.html.twig');
+    }
+}
