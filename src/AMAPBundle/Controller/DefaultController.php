d4afe3cb7ea60c9685197e5910861bbcc13c4791
Cocdesbois <corentin.fougeret@laposte.net>
2016-04-06 13:25:17 +0200

    Corentin
    Before manger

--- src/AMAPBundle/Controller/DefaultController.php
+++ src/AMAPBundle/Controller/DefaultController.php
@@ -11,28 +11,6 @@ class DefaultController extends Controller
 {
     public function indexAction(\Symfony\Component\HttpFoundation\Request $request)
     {
-        $em =  $this->getDoctrine()->getManager();
-       
-        
-        $form = $this->createFormBuilder()
-                ->setMethod('POST') 
-                ->add('libelleProduit', TextType::class)
-                ->add('quantite', IntegerType::class)
-                ->add('valider', SubmitType::class)
-                ->getForm();
-        if($form->handleRequest($request)->isSubmitted()){
-            $prod = new Produit();
-            $data = $form->getData();
-            $prod->setLibelle($data['libelleProduit']);
-            $prod->setQuantite($data['quantite']);
-            
-            $em->persist($prod);
-            $em->flush();
-                    
-        }
-         
-        $produit = $em->getRepository('AMAPBundle:Produit')
-                ->findAll();
-        return $this->render('AMAPBundle:Default:index.html.twig',array('prod'=>$produit,'form'=>$form->createView()));
+        return $this->render('AMAPBundle:Default:index.html.twig');
     }
 }
--- src/AMAPBundle/Controller/GestionProduitsController.php
+++ src/AMAPBundle/Controller/GestionProduitsController.php
@@ -11,28 +11,25 @@ class GestionProduitsController extends Controller
 {
     public function indexAction(\Symfony\Component\HttpFoundation\Request $request)
     {
-//        $em =  $this->getDoctrine()->getManager();
-//       
-//        
-//        $form = $this->createFormBuilder()
-//                ->setMethod('POST') 
-//                ->add('libelleProduit', TextType::class)
-//                ->add('quantite', IntegerType::class)
-//                ->add('valider', SubmitType::class)
-//                ->getForm();
-//        if($form->handleRequest($request)->isSubmitted()){
-//            $prod = new Produit();
-//            $data = $form->getData();
-//            $prod->setLibelle($data['libelleProduit']);
-//            $prod->setQuantite($data['quantite']);
-//            
-//            $em->persist($prod);
-//            $em->flush();
-//                    
-//        }
-//         
-//        $produit = $em->getRepository('AMAPBundle:Produit')
-//                ->findAll();
-        return $this->render('AMAPBundle:GestionProduits:gestionProduits.html.twig');
+        $em =  $this->getDoctrine()->getManager();
+        
+        $form = $this->createFormBuilder()
+                ->setMethod('POST') 
+                ->add('libelleProduit', TextType::class)
+                ->add('valider', SubmitType::class)
+                ->getForm();
+        
+        if($form->handleRequest($request)->isSubmitted()){
+            $prod = new Produit();
+            $data = $form->getData();
+            $prod->setLibelle($data['libelleProduit']);
+            $em->persist($prod);
+            $em->flush();     
+        }
+         
+        $produit = $em->getRepository('AMAPBundle:Produit')
+                ->findAll();
+        
+        return $this->render('AMAPBundle:GestionProduits:gestionProduits.html.twig', array('prod'=>$produit,'form'=>$form->createView()));
     }
 }
