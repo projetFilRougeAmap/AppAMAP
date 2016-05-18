c319c05808e406fe54c1f2090fc1dd4b545dbeb0
gakpa <gakpa@immonot.com>
2016-05-18 11:40:26 +0200

    home controller

---
+++ src/AMAPBundle/Controller/HomeController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace AMAPBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use \Symfony\Component\Form\Extension\Core\Type\TextType;
+use \Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use \Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use AMAPBundle\Entity\Produit;
+
+
+/**
+ * Produit controller.
+ *
+ */
+class HomeController extends Controller
+{
+    /**
+     * Affiche l'accueil
+     *
+     * @Route("/", name="amap_homepage")
+     * @Method({"GET", "POST"})
+     */
+    public function indexAction(\Symfony\Component\HttpFoundation\Request $request)
+    {
+        return $this->render('AMAPBundle:Default:index.html.twig');
+    }
+}
